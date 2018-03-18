560d065193afafa7ed34396583c877a8da4b1136
Sakthi Sankarraman <sakthi@Sakthis-iMac.local>
2018-03-18 10:21:43 -0700

    Adding common components and redux implementation.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import react from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
 
 const App = () => {
   return (
-    <Provider stire{createStore(reducers)}>
-      <View />
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Header headerText="Tech Stack" />
+      </View>
     </Provider>
   );
 };
